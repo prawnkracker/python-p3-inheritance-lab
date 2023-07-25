397e043f77992e4f4bff88b0ff0b992ce27b2b7d
Dennis Demir <dennisjdemir@gmail.com>
2023-07-25 19:51:13 -0400

    fixed lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -19,5 +19,5 @@ class Teacher(User):
             ]
     
     def teach(self):
-        return self.knowledge[random.randint(0,8)]
+        return self.knowledge[random.randint(0,7)]
         pass
